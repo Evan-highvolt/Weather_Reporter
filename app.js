5287e1affcf1bd424ede22fd331c746a072cb815
Evan-highvolt <evan.aljundi24@gmail.com>
2024-12-20 11:23:44 +0100

    Final commit

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const apiKey = "05bda04f6689661e9feba27a269b89a5";
+
+/**
+ * @constant {string} apiKey - Clé API nécessaire pour authentifier les requêtes à l'API OpenWeatherMap.
+ */
+const apiKey = "YOUR-API-CODE";
+
+/**
+ * @constant {string} apiURL - URL de base pour accéder à l'API OpenWeatherMap en utilisant des unités métriques.
+ */
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -6,22 +14,33 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 
 
+/**
+ * Fonction asynchrone permettant de vérifier la météo d'une ville donnée.
+ * @async
+ * @function checkWeather
+ * @param {string} city - Le nom de la ville pour laquelle vérifier la météo.
+ * @returns {Promise<void>} - Aucune valeur de retour explicite.
+ */
 async function checkWeather(city) {
     const response = await fetch(apiURL + city +`&appid=${apiKey}`);
 
+// Vérification des codes d'erreur (404: non trouvé, 400: requête incorrecte)
     if(response.status === 404 || response.status === 400) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     } else {
         var data = await response.json();
 
+        // Affichage des données dans la console pour le débogage
         console.log(data);
 
+        // Mise à jour des éléments HTML avec les données météorologiques
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+ "°c";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
+// Mise à jour de l'icône météo en fonction de la description principale de la météo
         if(data.weather[0].main === "Clouds") {
             weatherIcon.src = "./images/clouds.png"
 
@@ -34,7 +53,7 @@ async function checkWeather(city) {
         } else if(data.weather[0].main === "Mist") {
             weatherIcon.src = "images/mist.png"
         }
-
+// Affiche les informations météo et cache l'erreur si présente
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
 
@@ -44,6 +63,11 @@ async function checkWeather(city) {
     
     
 }
+
+/**
+ * Ajoute un écouteur d'événement au bouton de recherche.
+ * Lors du clic, la fonction checkWeather est appelée avec la valeur de l'input.
+ */
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
